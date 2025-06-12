4fc82a3395f4b7c5a8362da6788ffe78535e0e77
Ananth2k <ananth44rock@gmail.com>
2025-06-12 14:18:42 +0530

    changes

--- src/Home/Homeview.jsx
+++ src/Home/Homeview.jsx
@@ -28,26 +28,7 @@ function Home() {
         <svg className="right-arrow mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" ><path fill="none" d="m12 4 11.3 11.3a1 1 0 0 1 0 1.4L12 28"></path></svg>
       </div>
       <div className="home-body-cards py-3">
-        <Row>
-          {cardsData.map((card) => (
-            <Col key={card.id} lg={2} className="my-2 px-2 position-relative">
-              <div className="fav-svg">
-                 <svg   
-                 onClick={() => toggleFavorite(card.id)}
-                 fill={favorites[card.id] ? "#ff385c" : "rgba(0, 0, 0, 0.5)"}  
-                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" ><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg>
-              
-              </div>
-              <img
-                src={`/dummy-img${card.image}.png`}
-                alt={`dummy-${card.id}`}
-                className="home-card-img"
-              />
-             <Card />
-            </Col>
-            
-          ))}
-        </Row>
+       
         <div className="grid-card">
           
           {cardsData.map((card) => (
